ae2f0d9b52d7f4c3b4f8fcf5ce8a1cdd7d63b32e
JigneshMahadik <jigneshmahadik2898@gmail.com>
2024-03-19 01:00:28 +0530

    Changed API from boolean to multiple choice

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [ques, setQuestion] = useState([]);
 
   useEffect(()=>{
-    fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean")
+    // fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean")
+    fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple")
     .then((res) => {
       if (!res.ok) {
         throw new Error('Network response was not ok');
@@ -20,6 +21,7 @@ function App() {
       // const rec = data.results[0];
       // console.log(ques);
       // console.log(allData);
+      // console.log(ques.incorrect_answers);
     })
     .catch((error) => {
       console.error('There was a problem with the fetch operation:', error);
@@ -33,8 +35,19 @@ function App() {
       {
         <div>
           <h4>{ques.question}</h4>
-          <button>{ques.correct_answer}</button>&nbsp;&nbsp;&nbsp;&nbsp;
-          <button>{ques.incorrect_answers}</button>
+          <button>{ques.correct_answer}</button>
+          <br></br>
+          <br></br>
+          {
+            ques.incorrect_answers && ques.incorrect_answers.map((item,id)=>{
+              return(
+                <div>
+                  <button key={id}>{item}</button><br></br><br></br>
+                </div>
+              )
+            })
+          }
+          {/* <button>{ques.incorrect_answers}</button> */}
         </div>
         // allData.map((item,id)=>{
         //   return (
